49d997fc127ed5cc50d470bff3f8550a9b44f972
zloid <zloid-1@yandex.by>
2021-01-21 15:15:00 +0100

    fix(link): homepage

---
+++ build/precache-manifest.331d6d108d4ea8bdf56f2afd3fe241e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd59b41cd61bac732b3f547ccf01178d",
+    "url": "/React-App-Finding-Balance-Errors/index.html"
+  },
+  {
+    "revision": "a21ac0ee9dcc1b8aa2cd",
+    "url": "/React-App-Finding-Balance-Errors/static/css/2.c724ebb2.chunk.css"
+  },
+  {
+    "revision": "3975be904dfb5602445f",
+    "url": "/React-App-Finding-Balance-Errors/static/css/main.c5ae176c.chunk.css"
+  },
+  {
+    "revision": "a21ac0ee9dcc1b8aa2cd",
+    "url": "/React-App-Finding-Balance-Errors/static/js/2.6dae1551.chunk.js"
+  },
+  {
+    "revision": "3975be904dfb5602445f",
+    "url": "/React-App-Finding-Balance-Errors/static/js/main.32a0089c.chunk.js"
+  },
+  {
+    "revision": "919b3f4542faa5138905",
+    "url": "/React-App-Finding-Balance-Errors/static/js/runtime~main.de34318a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zloid/React-App-Finding-Balance-Errors/precache-manifest.fcee6360ccb87f7d44d7d4e82dd71655.js"
+  "/React-App-Finding-Balance-Errors/precache-manifest.331d6d108d4ea8bdf56f2afd3fe241e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zloid/React-App-Finding-Balance-Errors/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-App-Finding-Balance-Errors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
