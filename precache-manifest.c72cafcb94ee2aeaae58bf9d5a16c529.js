21445af817170b57e8ba3667b06d5c485fbca2af
zloid <zloid-1@yandex.by>
2021-01-21 09:48:04 +0100

    add: build folder to project

---
+++ precache-manifest.c72cafcb94ee2aeaae58bf9d5a16c529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf4f20fe33669f9738bd176d295d052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d0c1b6657269e413fd9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3f9931bb9e427368069c",
+    "url": "/static/css/main.c5ae176c.chunk.css"
+  },
+  {
+    "revision": "2d0c1b6657269e413fd9",
+    "url": "/static/js/2.5ff891b6.chunk.js"
+  },
+  {
+    "revision": "3f9931bb9e427368069c",
+    "url": "/static/js/main.e5ab7247.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
